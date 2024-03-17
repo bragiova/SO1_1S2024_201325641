439b31dfa817c385ae8b1c5519103b16ad5f5796
bragiova <bragiova.rivas@gmail.com>
2024-03-16 21:42:56 -0600

    Backend información cpu ram

--- Proyecto1/Backend/main.go
+++ Proyecto1/Backend/main.go
@@ -14,6 +14,8 @@ import (
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var conexion = ConexionMysql()
@@ -48,6 +50,16 @@ type CpuModel struct {
 	Cpu       float32   `json: "cpu"`
 }
 
+type RamModel struct {
+	Libre float32 `json: "libre"`
+	Uso   float32 `json "uso"`
+}
+
+type CpuRamHistorical struct {
+	Percentage float32 `json: "percentage"`
+	Date       string  `json "date"`
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	headers := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
@@ -56,8 +68,11 @@ func main() {
 	// router.Use(mux.CORSMethodMiddleware(router))
 
 	router.HandleFunc("/", home).Methods("GET")
+	router.HandleFunc("/realtime", cpuRamRealTime).Methods("GET")
+	router.HandleFunc("/historicalram", getRamHistorical).Methods("GET")
+	router.HandleFunc("/historicalcpu", getCpuHistorical).Methods("GET")
 	fmt.Println("Server running in 3000 port")
-	readCpuInfo()
+	go cpuRamRoutineDB()
 	http.ListenAndServe(":3000", handlers.CORS(headers, methods, origins)(router))
 }
 
@@ -65,7 +80,21 @@ func home(response http.ResponseWriter, request *http.Request) {
 	response.Write([]byte("Hello from Go api"))
 }
 
-func cpuRamRoutine() {
+func cpuRamRealTime(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+
+	var objCpu CpuModel = readCpuInfo()
+	var ramInfo = readRamInfo()
+
+	var dataReal DataUsage
+
+	dataReal.Cpu = objCpu.Cpu
+	dataReal.Ram = ramInfo
+
+	json.NewEncoder(response).Encode(dataReal)
+}
+
+func cpuRamRoutineDB() {
 	// Lanzar un goroutine que ejecute la función cada n segundos
 	interval := 10 // segundos
 	ticker := time.NewTicker(time.Second * time.Duration(interval))
@@ -74,12 +103,20 @@ func cpuRamRoutine() {
 	for {
 		select {
 		case <-ticker.C:
-			readCpuInfo()
+			var objCpu CpuModel = readCpuInfo()
+			if objCpu.Cpu > 0 {
+				saveDBCpuRamInfo(objCpu.Cpu, true)
+			}
+
+			var ramInfo = readRamInfo()
+			if ramInfo >= 0 {
+				saveDBCpuRamInfo(ramInfo, false)
+			}
 		}
 	}
 }
 
-func readCpuInfo() {
+func readCpuInfo() CpuModel {
 	fmt.Println("DATOS OBTENIDOS DESDE EL MODULO CPU:")
 	fmt.Println("")
 	var cpu_obj CpuModel
@@ -93,22 +130,37 @@ func readCpuInfo() {
 	if err := cmd.Run(); err != nil {
 		fmt.Println("Error al ejecutar el comando:", err)
 		fmt.Println("Mensaje de error:", stderr.String())
-		return
+		return cpu_obj
 	}
 
 	if err := json.Unmarshal(stdout.Bytes(), &cpu_obj); err != nil {
 		fmt.Println("Error al decodificar JSON:", err)
-		return
+		return cpu_obj
 	}
-	fmt.Println(cpu_obj.Cpu)
+	// fmt.Println(cpu_obj.Cpu)
+	return cpu_obj
 }
 
-func readRamInfo() {
-	fmt.Println("DATOS OBTENIDOS DESDE EL MODULO CPU:")
-	fmt.Println("")
-	var cpu_obj CpuModel
+func saveDBCpuRamInfo(percentage float32, isCpu bool) {
+	var timeNow = time.Now().Format("2006-01-02 15:04:05")
 
-	cmd := exec.Command("sh", "-c", "cat /proc/cpu_so1_1s2024")
+	tableName := (map[bool]string{true: "cpu_info", false: "ram_info"})[isCpu]
+
+	query := "INSERT INTO " + tableName + "(porcentaje, fecha) VALUES (?,?);"
+	result, err := conexion.Exec(query, percentage, timeNow)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(result)
+}
+
+func readRamInfo() float32 {
+	fmt.Println("DATOS OBTENIDOS DESDE EL MODULO RAM:")
+	fmt.Println("")
+	var ram_obj RamModel
+
+	cmd := exec.Command("sh", "-c", "cat /proc/ram_so1_1s2024")
 
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
@@ -117,14 +169,19 @@ func readRamInfo() {
 	if err := cmd.Run(); err != nil {
 		fmt.Println("Error al ejecutar el comando:", err)
 		fmt.Println("Mensaje de error:", stderr.String())
-		return
+		return 0
 	}
 
-	if err := json.Unmarshal(stdout.Bytes(), &cpu_obj); err != nil {
+	if err := json.Unmarshal(stdout.Bytes(), &ram_obj); err != nil {
 		fmt.Println("Error al decodificar JSON:", err)
-		return
+		return 0
 	}
-	fmt.Println(cpu_obj.Cpu)
+	// fmt.Println(ram_obj)
+
+	totalRam := ram_obj.Libre + ram_obj.Uso
+	percentageRam := (ram_obj.Uso / totalRam) * 100
+
+	return percentageRam
 }
 
 func ConexionMysql() *sql.DB {
@@ -149,4 +206,46 @@ func ConexionMysql() *sql.DB {
 		fmt.Println("Conexion con MySQL Correcta")
 	}
 	return conexion
+}
+
+func getRamHistorical(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+	var lista []CpuRamHistorical
+	query := "select porcentaje, fecha from ram_info;"
+	result, err := conexion.Query(query)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for result.Next() {
+		var logc CpuRamHistorical
+
+		err = result.Scan(&logc.Percentage, &logc.Date)
+		if err != nil {
+			fmt.Println(err)
+		}
+		lista = append(lista, logc)
+	}
+	json.NewEncoder(response).Encode(lista)
+}
+
+func getCpuHistorical(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+	var lista []CpuRamHistorical
+	query := "select porcentaje, fecha from cpu_info;"
+	result, err := conexion.Query(query)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for result.Next() {
+		var logc CpuRamHistorical
+
+		err = result.Scan(&logc.Percentage, &logc.Date)
+		if err != nil {
+			fmt.Println(err)
+		}
+		lista = append(lista, logc)
+	}
+	json.NewEncoder(response).Encode(lista)
 }
