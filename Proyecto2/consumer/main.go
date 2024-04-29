99cdfc180feba2fc3c29037a46378d6507fa84e6
bragiova <bragiova.rivas@gmail.com>
2024-04-28 19:00:41 -0600

    Cambios redis y mongo

--- Proyecto2/consumer/main.go
+++ Proyecto2/consumer/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
-	"os/signal"
-	"syscall"
+	"strconv"
+	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/go-redis/redis/v8"
 	"github.com/joho/godotenv"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Vote struct {
@@ -16,6 +22,114 @@ type Vote struct {
 	Album string `json:"album"`
 	Year  string `json:"year"`
 	Rank  string `json:"rank"`
+}
+
+func sendInfoRedis(voteBand string, rdb *redis.Client) {
+	var voto Vote
+	err := json.Unmarshal([]byte(voteBand), &voto)
+	if err != nil {
+		fmt.Printf("Error al decodificar el JSON: %v\n", err)
+		return
+	}
+
+	// Rank
+	saveRankeds(voto, rdb)
+
+}
+
+func saveRankeds(vote Vote, rdb *redis.Client) {
+	fmt.Printf("SaveRankeds Redis:")
+	// Convertir el rank a float64
+	rank, err := strconv.ParseFloat(vote.Rank, 64)
+	if err != nil {
+		fmt.Println("Error al convertir string a float64:", err)
+		return
+	}
+
+	// context, hash, field, value
+	// Guarda incrementa el valor del rank a un álbum, es una sumatoria de ranks por álbum
+	err = rdb.HIncrByFloat(context.Background(), "Rank", vote.Album, rank).Err()
+	if err != nil {
+		fmt.Printf("Error al enviar datos Rank Redis: %v\n", err)
+		return
+	}
+
+	// context, hash, field, value
+	// Guarda el álbum con su rank
+	err = rdb.HSet(context.Background(), "Album", vote.Album, rank).Err()
+	if err != nil {
+		fmt.Printf("Error al enviar datos Album Redis: %v\n", err)
+		return
+	}
+
+	// Incrementa cantidad de votos por banda
+	err = rdb.HIncrBy(context.Background(), "Band", vote.Name, 1).Err()
+	if err != nil {
+		fmt.Printf("Error al enviar datos Band Redis: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Enviado a Redis: %+v\n", vote)
+}
+
+func calcAverageAlbum(rdb *redis.Client) {
+	// Obtener todos los valores almacenados con el hash "Album"
+	albumCount, err := rdb.HGetAll(context.Background(), "Album").Result()
+	if err != nil {
+		fmt.Printf("Error al obtener los valores de Redis: %v\n", err)
+		return
+	}
+
+	// Calcular el promedio para cada álbum
+	for album, rank := range albumCount {
+		rank64, err := strconv.ParseFloat(rank, 64)
+		if err != nil {
+			fmt.Printf("Error al convertir el valor a float64: %v\n", err)
+			continue
+		}
+		// Promedio
+		average := rank64 / float64(len(albumCount))
+
+		// Guardar el promedio de cada álbum
+		err = rdb.HSet(context.Background(), "Average", album, average).Err()
+		if err != nil {
+			fmt.Printf("Error al enviar datos Average Redis: %v\n", err)
+			continue
+		}
+	}
+}
+
+func saveDataMongo(vote string) {
+	uriMongo := os.Getenv("MONGO_URI")
+
+	// Crear cliente MongoDB
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uriMongo))
+	if err != nil {
+		fmt.Printf("Error al conectar a MongoDB: %v\n", err)
+		return
+	}
+
+	defer client.Disconnect(context.Background())
+
+	// Obtenemos colección de logs
+	collection := client.Database("dbp2").Collection("logs")
+
+	currentDate := time.Now()
+
+	stringLog := map[string]interface{}{
+		"fecha": currentDate.Format("2006-01-02"),
+		"hora":  currentDate.Format("15:04:05"),
+		"voto":  vote,
+	}
+
+	// Insertar el documento en la colección
+	_, err = collection.InsertOne(context.Background(), stringLog)
+	if err != nil {
+		fmt.Printf("Error al insertar log MongoDB: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Log enviado a MongoDB: %v\n", stringLog)
 }
 
 func main() {
@@ -39,33 +153,41 @@ func main() {
 		log.Fatalf("Error al crear el consumidor: %s\n", err)
 	}
 
+	// Cerrar el consumidor
+	defer consumer.Close()
+
 	// Suscribirse a un tema (topic)
-	err = consumer.SubscribeTopics([]string{topicKafka}, nil)
+	err = consumer.Subscribe(topicKafka, nil)
 	if err != nil {
 		log.Fatalf("Error al suscribirse al topic: %s\n", err)
 	}
 
-	// Manejar señales de interrupción
-	sigchan := make(chan os.Signal, 1)
-	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+	// Conectar Redis
+	hostRedis := os.Getenv("REDIS_HOST")
+	passRedis := os.Getenv("REDIS_PASS")
+	// addressRedis := hostRedis + ":6379"
 
-	run := true
-	for run == true {
-		select {
-		case sig := <-sigchan:
-			log.Printf("Terminando: %v", sig)
-			run = false
-		default:
-			// Consumir mensajes
-			msg, err := consumer.ReadMessage(-1)
-			if err == nil {
-				fmt.Printf("Mensaje recibido: %s\n", string(msg.Value))
-			} else {
-				log.Printf("Error al recibir mensaje: %v\n", err)
-			}
+	// Crear cliente Redis
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     hostRedis + ":6379",
+		Password: passRedis,
+		DB:       0,
+	})
+
+	// Cerrar la conexión redis
+	defer rdb.Close()
+
+	// Consumir mensajes
+	for {
+		msg, err := consumer.ReadMessage(-1)
+		if err == nil {
+			fmt.Printf("Mensaje recibido: %s\n", string(msg.Value))
+			sendInfoRedis(string(msg.Value), rdb)
+			calcAverageAlbum(rdb)
+			saveDataMongo(string(msg.Value))
+		} else {
+			log.Printf("Error al recibir mensaje: %v\n", err)
+			break
 		}
 	}
-
-	// Cerrar el consumidor
-	consumer.Close()
 }
